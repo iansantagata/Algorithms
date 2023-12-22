e300bc9c3accb44a74722b5f1e8dc2f26e586709
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2023-12-22 18:55:02 -0500

    Make it clear tests are testing bubble sort

--- tests/sorting/test_bubble.py
+++ tests/sorting/test_bubble.py
@@ -1,5 +1,5 @@
 import pytest
-from sorting.bubble import sort
+from sorting.bubble import sort as bubble_sort
 from tests.helpers.assertions import assert_equivalent_lists
 
 from typing import NoReturn
@@ -7,21 +7,21 @@ from typing import NoReturn
 
 def test_sort_ordered_list(ordered_list: list) -> NoReturn:
     
-    output: list = sort(input=ordered_list)
+    output: list = bubble_sort(input=ordered_list)
     expected: list = ordered_list
 
     assert_equivalent_lists(output, expected)
 
 def test_sort_revered_list(reversed_list: list, ordered_list: list) -> NoReturn:
 
-    output: list = sort(input=reversed_list)
+    output: list = bubble_sort(input=reversed_list)
     expected: list = ordered_list
 
     assert_equivalent_lists(output, expected)
 
 def test_sort_duplicate_list(duplicate_list: list) -> NoReturn:
 
-    output: list = sort(input=duplicate_list)
+    output: list = bubble_sort(input=duplicate_list)
     # Cannot use ordered_list as expected output here because it was not fully used to build the duplicate list
     expected: list = sorted(duplicate_list)
 
@@ -29,21 +29,21 @@ def test_sort_duplicate_list(duplicate_list: list) -> NoReturn:
 
 def test_sort_shuffled_list(shuffled_list: list, ordered_list: list) -> NoReturn:
 
-    output: list = sort(input=shuffled_list)
+    output: list = bubble_sort(input=shuffled_list)
     expected: list = ordered_list
 
     assert_equivalent_lists(output, expected)
 
 def test_partially_ordered_list(partially_ordered_list: list, ordered_list: list) -> NoReturn:
 
-    output: list = sort(input=partially_ordered_list)
+    output: list = bubble_sort(input=partially_ordered_list)
     expected: list = ordered_list
 
     assert_equivalent_lists(output, expected)
 
 def test_random_list(random_list: list) -> NoReturn:
 
-    output: list = sort(input=random_list)
+    output: list = bubble_sort(input=random_list)
     # Cannot use ordered_list as expected output here because it was not fully used to build the random list
     expected: list = sorted(random_list)
 
