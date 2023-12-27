f082d7f0184aef4a47a76c66898888ed84de884e
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2023-12-26 20:50:33 -0500

    Adjust comments in bubble sort algorithm

--- src/sorting/bubble.py
+++ src/sorting/bubble.py
@@ -1,7 +1,5 @@
 """
-Bubble Sort
-
-More Details - https://en.wikipedia.org/wiki/Bubble_sort
+Bubble Sort https://en.wikipedia.org/wiki/Bubble_sort
 """
 
 
@@ -9,12 +7,25 @@ def sort(data: list[int]) -> list[int]:
     """
     Bubble Sort - https://en.wikipedia.org/wiki/Bubble_sort
 
-    Memory Usage: O(1)
-
     Performance:
     - Worst Case: O(n²)
     - Average Case: O(n²)
     - Best Case: O(n)
+
+    Memory Usage: O(1)
+
+    Algorithm:
+
+    Basically, starting with the first element and considering pairs of elements, move
+    the bigger element of the pair toward the end by swapping the values.
+
+    Continue like this, moving down the list, pair by pair, until the end of the list is
+    reached.  At this point, the last element of the list is necessarily the greatest
+    value in the whole list.
+
+    Start again with the first element, and compare and swap pairs in a similar manner
+    again until either no swaps have been made or the part of the list that
+    has been sorted is reached.
     """
     num_elements: int = len(data)
 
@@ -26,7 +37,8 @@ def sort(data: list[int]) -> list[int]:
     for i in range(num_elements):
         swap_occurred: bool = False
 
-        # The last i elements are already in their proper place
+        # The last i elements are already in their proper place, so we can stop at that
+        # point rather than needing to check every element again.
         for j in range(1, num_elements - i):
             # Compare one earlier element to the current element and switch the values
             # if they are out of order
@@ -41,6 +53,8 @@ def sort(data: list[int]) -> list[int]:
 
                 swap_occurred = True
 
+        # If we did not swap at all from i through the end of the unsorted portion of
+        # the list, then we know that the list is now entirely sorted!
         if not swap_occurred:
             break
 
